2177135d8e60348290881af27b9a57f8397d14a9
Shiva Choudhary <shivachaudhary537@gmail.com>
2021-04-03 09:40:41 +0530

    init commit

---
+++ Public/js/app.js
@@ -0,0 +1,25 @@
+
+const weatherform = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+//const messageTwo = document.querySelector('#message-2')
+
+messageOne.textContent = 'Loading...'
+weatherform.addEventListener('submit', (event) => 
+{    
+     event.preventDefault()   
+     const location = search.value
+     const url = 'http://localhost:3000/weather?address=' + location 
+      
+  
+     fetch(url).then((response) => {
+      response.json().then((data) => {
+          if(data.error){
+              messageOne.textContent = data.error
+          }else{
+              messageOne.textContent = data.Data
+          }
+      })
+})
+
+})
